22fc3fe3c64cce18c9bec95beda6374bb4852cf6
Afakari <a.fakari@tapsell.com>
2025-02-14 19:49:50 +0330

    add more stuff to the setup scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
-
 set -e
+
+# Variables
+CONFIG_REPO="https://github.com/afakari/dotfiles.git"
+CONFIG_DIR="$HOME/configs_repo"
+USER_HOME=$(eval echo ~$(logname))
+
+# Argument flags
+NO_DNS=false
+NO_GOLANG=false
+NO_DOCKER=false
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --no-dns)
+            NO_DNS=true
+            shift
+            ;;
+        --no-golang)
+            NO_GOLANG=true
+            shift
+            ;;
+        --no-docker)
+            NO_DOCKER=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
 
 check_prerequisites() {
     echo "Checking prerequisites..."
-    
 
     if [[ $EUID -ne 0 ]]; then
         echo "This script must be run as root. Use sudo to execute the script."
         exit 1
     fi
-
 
     if ! ping -c 1 google.com &> /dev/null; then
         echo "Internet connection is required. Please check your connection and try again."
@@ -51,6 +80,11 @@ EOL
 }
 
 setup_dns() {
+    if $NO_DNS; then
+        echo "Skipping DNS setup as per --no-dns flag."
+        return
+    fi
+
     echo "Setting up DNS..."
     dns_servers=("178.22.122.100" "185.51.200.2")
 
@@ -73,16 +107,15 @@ setup_dns() {
     fi
 }
 
-
 update_system() {
     echo "Updating system..."
-    sudo apt update -y && sudo apt upgrade -y
+    apt update -qq -y && apt upgrade -qq -y
 }
 
 install_package() {
     echo "Installing $1..."
     if ! dpkg -l | grep -qw "$1"; then
-        sudo apt install -y $1 > /dev/null
+        apt install -qq -y $1 > /dev/null
         echo "$1 installation completed."
     else
         echo "$1 is already installed. Skipping installation."
@@ -94,8 +127,69 @@ install_deb_package() {
     local deb_file=${url##*/}
     echo "Downloading and installing $deb_file..."
     wget -q $url -O /tmp/$deb_file
-    sudo dpkg -i /tmp/$deb_file || sudo apt install -f -y
+    dpkg -i /tmp/$deb_file > /dev/null 2>&1 || apt install -f -y > /dev/null 2>&1
     rm /tmp/$deb_file
+}
+
+install_golang() {
+    if $NO_GOLANG; then
+        echo "Skipping Go installation as per --no-golang flag."
+        return
+    fi
+
+    echo "Installing Go..."
+    wget -c https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+    export PATH=$PATH:/usr/local/go/bin
+    source ~/.profile
+    go version || {
+        echo "Go installation failed. Please check logs.";
+    }
+}
+
+install_docker() {
+    if $NO_DOCKER; then
+        echo "Skipping Docker installation as per --no-docker flag."
+        return
+    fi
+
+    echo "Installing Docker..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null 2>&1
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt update -qq -y
+    apt-get install -qq -y docker-ce docker-ce-cli containerd.io > /dev/null
+    docker --version || {
+        echo "Docker installation failed. Please check logs.";
+    }
+}
+
+setup_oh_my_zsh() {
+    if [ ! -d "$USER_HOME/.oh-my-zsh" ]; then
+        echo "Installing Oh My Zsh..."
+        sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || {
+            echo "Oh My Zsh installation failed. Please check logs.";
+        }
+    else
+        echo "Oh My Zsh is already installed. Skipping installation."
+    fi
+}
+
+copy_config_files() {
+    echo "Downloading configuration files..."
+    if [ -d "$CONFIG_DIR" ]; then
+        echo "Configuration directory already exists. Updating..."
+        git -C $CONFIG_DIR pull
+    else
+        git clone $CONFIG_REPO $CONFIG_DIR
+    fi
+
+    echo "Copying configuration files..."
+    cp -r $CONFIG_DIR/dotfiles/zshrc $USER_HOME/.zshrc
+    cp -r $CONFIG_DIR/dotfiles/tmux.conf $USER_HOME/.tmux.conf
+    mkdir -p $USER_HOME/.local/kitty
+    cp -r $CONFIG_DIR/dotfiles/kitty.conf $USER_HOME/.local/kitty/kitty.conf
+
+    # Ensure the user owns their home directory files
+    chown -R $(logname):$(logname) $USER_HOME
 }
 
 echo "Setting up your environment..."
@@ -104,13 +198,10 @@ check_prerequisites
 set_iranian_repos
 update_system
 
-packages=(apt-transport-https  ca-certificates lsb-release  xclip python3 python3-pip vim zsh tmux kitty tor git wget fzf autojump )
+packages=(apt-transport-https ca-certificates lsb-release xclip python3 python3-pip vim zsh tmux kitty tor git wget fzf autojump)
 for package in "${packages[@]}"; do
     install_package $package
 done
-
-
-
 
 if ! command -v code &> /dev/null; then
     install_deb_package "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
@@ -124,49 +215,9 @@ else
     echo "Google Chrome is already installed. Skipping installation."
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Installing Oh My Zsh..."
-    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || {
-        echo "Oh My Zsh installation failed. Please check logs.";
-    }
-else
-    echo "Oh My Zsh is already installed. Skipping installation."
-fi
-
-
-# echo "Installing Go..."
-# wget -c https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-# export PATH=$PATH:/usr/local/go/bin
-# source ~/.profile
-# go version || {
-#     echo "Go installation failed. Please check logs.";
-# }
-
-
-# echo "Installing Docker..."
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt update -y
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-# docker --version || {
-#     echo "Docker installation failed. Please check logs.";
-# }
-
-
-echo "Downloading configuration files..."
-config_repo="https://github.com/afakari/dotfiles.git"
-config_dir="$HOME/configs_repo"
-
-if [ -d "$config_dir" ]; then
-    echo "Configuration directory already exists. Updating..."
-    git -C $config_dir pull
-else
-    git clone $config_repo $config_dir
-fi
-
-echo "Copying configuration files..."
-cp -r $config_dir/dotfiles/zshrc ~/.zshrc
-cp -r $config_dir/dotfiles/tmux.conf ~/.tmux.conf
-cp -r $config_dir/dotfiles/kitty.conf ~/.local/kitty/kitty.conf
+setup_oh_my_zsh
+install_golang
+install_docker
+copy_config_files
 
 echo "Environment setup complete! You may need to log out and log back in for changes to take effect."
